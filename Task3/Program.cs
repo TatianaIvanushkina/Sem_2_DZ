95d9c3a5f721b2ebbff35cdbbade76a48144ee9a
Tatiana Ivanyshkina <Ita86@bk.ru>
2024-02-01 22:41:01 +0400

    Finish Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+
+int a = 17;
+int thirstDigit = a / 10;
+int secondDigit = a % 10;
+if (thirstDigit > secondDigit)
+{
+    Console.WriteLine ($"Naibolshaya cifra {thirstDigit}");
+}
+else
+{
+    Console.WriteLine ($"Naibolshaya cifra {secondDigit}");
+}
