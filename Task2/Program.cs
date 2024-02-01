d62a77e2d46c8251bac834f3d5f716c663914a05
Tatiana Ivanyshkina <Ita86@bk.ru>
2024-02-01 22:34:38 +0400

    Finish Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+int X = 4;
+int Y = -2;
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine ("1 chetvert");
+}
+if (X < 0 && Y > 0)
+{
+    Console.WriteLine ("2 chetvert");
+}
+if (X < 0 && Y < 0)
+{
+    Console.WriteLine ("3 chetvert");
+}
+if (X > 0 && Y < 0)
+{
+    Console.WriteLine ("4 chetvert");
+}
